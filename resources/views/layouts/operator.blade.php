314d29d60b90a98d42c71f0ec365bde52f6d8cb3
3rr0ll~ <purinochristian@gmail.com>
2025-09-14 22:38:30 +0800

    Update operator layout to include background color for main content area

--- resources/views/layouts/operator.blade.php
+++ resources/views/layouts/operator.blade.php
@@ -187,7 +187,7 @@
         </div>
     </aside>
     {{-- Main content --}}
-    <div class="p-4 sm:ml-64 mt-16">
+    <div class="p-4 sm:ml-64 mt-16 bg-[#e5e5e4]" style="min-height: 100vh;">
         {{-- This will render the "header" section if defined in the child view --}}
         @hasSection('header')
             <div>
