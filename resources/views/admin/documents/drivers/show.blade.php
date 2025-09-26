89aef90f755d9d264c2f0d51037e7e2465474ae7
3rr0ll~ <purinochristian@gmail.com>
2025-09-26 23:29:25 +0800

    change condition for empty handling

--- resources/views/admin/documents/drivers/show.blade.php
+++ resources/views/admin/documents/drivers/show.blade.php
@@ -81,7 +81,7 @@
                     </tr>
                 </thead>
                 <tbody class="bg-white divide-y divide-gray-200">
-                    @forelse($documents as $i => $document)
+                    @foreach($documents as $i => $document)
                     <tr>
                         <td class="px-4 py-2 whitespace-nowrap">{{ $i + 1 }}</td>
                         <td class="px-4 py-2 whitespace-nowrap">{{ $document->documentType->name }}</td>
@@ -109,19 +109,8 @@
                             </x-button>
                         </td>
                     </tr>
-                    @empty
-                    <tr>
-                        <td colspan="4" class="px-4 py-8 text-center text-gray-500">
-                            <div class="flex flex-col items-center">
-                                <svg class="mx-auto h-12 w-12 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
-                                </svg>
-                                <h3 class="mt-2 text-sm font-medium text-gray-900">No documents</h3>
-                                <p class="mt-1 text-sm text-gray-500">No documents have been submitted by this driver yet.</p>
-                            </div>
-                        </td>
-                    </tr>
-                    @endforelse
+                 
+                    @endforeach
                 </tbody>
             </table>
         </div>
