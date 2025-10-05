66f664e3c9a23b71f129ae85375dadc310d8c76a
3rr0ll~ <purinochristian@gmail.com>
2025-10-05 17:07:29 +0800

    or number applied

--- app/Http/Controllers/Admin/CertificateController.php
+++ app/Http/Controllers/Admin/CertificateController.php
@@ -2,22 +2,29 @@
 
 namespace App\Http\Controllers\Admin;
 
-
 use App\Http\Controllers\Controller;
 use App\Models\MotorDetail;
+use App\Models\Payment;
 use Illuminate\Http\Request;
 use Barryvdh\DomPDF\PDF;
 use App\Helpers\ActivityLogger;
 use Illuminate\Support\Facades\Auth;
 
-
 class CertificateController extends Controller
 {
-    public function generateMTOP($motorDetailId)
+    public function previewMTOP($motorDetailId, Request $request)
     {
-        $motorDetail = MotorDetail::with(['franchiseApplication.operator', 'franchiseApplication.driver', 'franchiseApplication.route', 'unitMake'])->findOrFail($motorDetailId);
+        $motorDetail = MotorDetail::with([
+            'franchiseApplication.operator',
+            'franchiseApplication.driver',
+            'franchiseApplication.route',
+            'unitMake'
+        ])->findOrFail($motorDetailId);
 
-        // Auto-fill data from database
+        // Get OR number from modal input
+        $orNo = $request->query('or_no');
+        $payment = $orNo ? Payment::where('or_no', $orNo)->first() : null;
+
         $franchiseApplication = $motorDetail->franchiseApplication;
         $operator = $franchiseApplication->operator;
         $driver = $franchiseApplication->driver;
@@ -32,42 +39,8 @@ class CertificateController extends Controller
             'franchise_no' => $franchiseApplication->franchise_no ?? $motorDetail->franchise_number ?? 'N/A',
             'sticker_no' => $franchiseApplication->sticker_no ?? $motorDetail->sticker_number ?? 'N/A',
             'case_no' => $motorDetail->case_number ?? 'N/A',
-            'or_no' => $motorDetail->or_number ?? 'N/A',
-            'amount' => $franchiseApplication->franchise_fee ?? $motorDetail->amount ?? 'N/A',
-            'ctc_no' => $franchiseApplication->ctc_no ?? 'N/A',
-            'date_issued' => $motorDetail->date_issued
-                ? \Carbon\Carbon::parse($motorDetail->date_issued)->format('F d, Y')
-                : now()->format('F d, Y'),
-            'place_issued' => $motorDetail->place_issued ?? 'Padre Garcia, Batangas',
-            'validity' => $motorDetail->validity ?? $franchiseApplication->franchise_end_date ?? now()->addYear()->format('Y-m-d')
-        ];
-
-        $pdf = app('dompdf.wrapper')->loadView('admin.certificates.MTOP', $data);
-        return $pdf->download('MTOP_' . ($data['franchise_no'] !== 'N/A' ? $data['franchise_no'] : 'Motor_' . $motorDetail->id) . '.pdf');
-    }
-
-
-    public function previewMTOP($motorDetailId)
-    {
-        $motorDetail = MotorDetail::with(['franchiseApplication.operator', 'franchiseApplication.driver', 'franchiseApplication.route', 'unitMake'])->findOrFail($motorDetailId);
-
-        // Auto-fill data from database
-        $franchiseApplication = $motorDetail->franchiseApplication;
-        $operator = $franchiseApplication->operator;
-        $driver = $franchiseApplication->driver;
-        $route = $franchiseApplication->route;
-
-        $data = [
-            'motorDetail' => $motorDetail,
-            'franchiseApplication' => $franchiseApplication,
-            'operator' => $operator,
-            'driver' => $driver,
-            'route' => $route,
-            'franchise_no' => $franchiseApplication->franchise_no ?? $motorDetail->franchise_number ?? 'N/A',
-            'sticker_no' => $franchiseApplication->sticker_no ?? $motorDetail->sticker_number ?? 'N/A',
-            'case_no' => $motorDetail->case_number ?? 'N/A',
-            'or_no' => $motorDetail->or_number ?? 'N/A',
-            'amount' => $franchiseApplication->franchise_fee ?? $motorDetail->amount ?? 'N/A',
+            'or_no' => $payment?->or_no ?? $motorDetail->or_number ?? 'N/A',
+            'amount' => $payment?->amount_paid ?? $franchiseApplication->franchise_fee ?? $motorDetail->amount ?? 'N/A',
             'ctc_no' => $franchiseApplication->ctc_no ?? 'N/A',
             'date_issued' => $motorDetail->date_issued
                 ? \Carbon\Carbon::parse($motorDetail->date_issued)->format('F d, Y')
@@ -79,11 +52,18 @@ class CertificateController extends Controller
         return view('admin.certificates.MTOP', $data);
     }
 
-    public function previewMayorsPermit($motorDetailId)
+    public function previewMayorsPermit($motorDetailId, Request $request)
     {
-        $motorDetail = MotorDetail::with(['franchiseApplication.operator', 'franchiseApplication.driver', 'franchiseApplication.route', 'unitMake'])->findOrFail($motorDetailId);
+        $motorDetail = MotorDetail::with([
+            'franchiseApplication.operator',
+            'franchiseApplication.driver',
+            'franchiseApplication.route',
+            'unitMake'
+        ])->findOrFail($motorDetailId);
 
-        // Auto-fill data from database
+        $orNo = $request->query('or_no');
+        $payment = $orNo ? Payment::where('or_no', $orNo)->first() : null;
+
         $franchiseApplication = $motorDetail->franchiseApplication;
         $operator = $franchiseApplication->operator;
         $driver = $franchiseApplication->driver;
@@ -96,8 +76,8 @@ class CertificateController extends Controller
             'driver' => $driver,
             'route' => $route,
             'franchise_no' => $franchiseApplication->franchise_no ?? $motorDetail->franchise_number ?? 'N/A',
-            'or_no' => $motorDetail->or_number ?? 'N/A',
-            'amount' => $franchiseApplication->franchise_fee ?? $motorDetail->amount ?? 'N/A',
+            'or_no' => $payment?->or_no ?? $motorDetail->or_number ?? 'N/A',
+            'amount' => $payment?->amount_paid ?? $franchiseApplication->franchise_fee ?? $motorDetail->amount ?? 'N/A',
             'ctc_no' => $franchiseApplication->ctc_no ?? 'N/A',
             'date_issued' => $motorDetail->date_issued
                 ? \Carbon\Carbon::parse($motorDetail->date_issued)->format('F d, Y')
@@ -108,11 +88,18 @@ class CertificateController extends Controller
         return view('admin.certificates.mayors_permit', $data);
     }
 
-    public function previewApplication($motorDetailId)
+    public function previewApplication($motorDetailId, Request $request)
     {
-        $motorDetail = MotorDetail::with(['franchiseApplication.operator', 'franchiseApplication.driver', 'franchiseApplication.route', 'unitMake'])->findOrFail($motorDetailId);
+        $motorDetail = MotorDetail::with([
+            'franchiseApplication.operator',
+            'franchiseApplication.driver',
+            'franchiseApplication.route',
+            'unitMake'
+        ])->findOrFail($motorDetailId);
 
-        // Auto-fill data from database
+        $orNo = $request->query('or_no');
+        $payment = $orNo ? Payment::where('or_no', $orNo)->first() : null;
+
         $franchiseApplication = $motorDetail->franchiseApplication;
         $operator = $franchiseApplication->operator;
         $driver = $franchiseApplication->driver;
@@ -128,7 +115,9 @@ class CertificateController extends Controller
             'sticker_no' => $franchiseApplication->sticker_no ?? $motorDetail->sticker_number ?? 'N/A',
             'toda_president' => $motorDetail->toda_president ?? 'N/A',
             'traffic_division' => $motorDetail->traffic_division ?? 'N/A',
-            'pfuc_chairperson' => $motorDetail->pfuc_chairperson ?? 'N/A'
+            'pfuc_chairperson' => $motorDetail->pfuc_chairperson ?? 'N/A',
+            'or_no' => $payment?->or_no ?? 'N/A',
+            'amount' => $payment?->amount_paid ?? 'N/A'
         ];
 
         return view('admin.certificates.application', $data);
@@ -136,8 +125,7 @@ class CertificateController extends Controller
 
     public function logPrint(Request $request, $motorDetailId)
     {
-        $motorDetail = MotorDetail::with('franchiseApplication.operator')
-            ->findOrFail($motorDetailId);
+        $motorDetail = MotorDetail::with('franchiseApplication.operator')->findOrFail($motorDetailId);
 
         $franchiseApplication = $motorDetail->franchiseApplication;
         $operator = $franchiseApplication?->operator;
